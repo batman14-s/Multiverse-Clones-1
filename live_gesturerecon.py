2824abe3b42746217211914bb8189aebf376f690
LazyXghost <B20123@students.iitmandi.ac.in>
2021-09-28 14:38:02 +0530

    Made live gesture recognition better

--- live_gesturerecon.py
+++ live_gesturerecon.py
@@ -1,45 +1,84 @@
 from keras.models import load_model
 import cv2
 import numpy as np
+import time
+from datetime import datetime
+
+def getTime():
+    time = datetime.now().hour*3600+datetime.now().minute*60 + datetime.now().second + datetime.now().microsecond/1000000
+    return time
 
 def getCorrectClass(tmpClass,classMap):
   for key, value in classMap.items():
          if value == tmpClass:
              return key
 
-model = load_model("FinalModel.h5")
-classMap = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, 'EQUALS':6, 'NONE':7, 'SUM':8}
-cap = cv2.VideoCapture(0)
-prev_move = None
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        continue
-    # rectangle for user to play
-    cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
-    # extract the region of image within the user rectangle
-    roi = frame[100:500, 100:500]
-    # img = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-    img2 = cv2.Canny(roi, 50, 150)
-    imageSize = 100
-    img = cv2.resize(img2, (imageSize, imageSize))
-    # predict the move made
-  
-    pred = model.predict(img.reshape(1, img.shape[0], img.shape[1], 1))
-    pred_cls = np.argmax(pred)
-    symbol= getCorrectClass(pred_cls,classMap)
-    print(symbol)
+def getCannyImage(img,lowI,highI):
+    return cv2.Canny(img, lowI, highI)
 
-    # display the information
-    font=cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img2, " outcome: " + str(symbol),
-                (imageSize, imageSize), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
-   
-    cv2.imshow('frame', img2)
-
+def keyCheck():
     k=cv2.waitKey(10)
     if k == ord('q'):
-        break
+        cap.release()
+        cv2.destroyAllWindows()
+        exit()
+    if k == ord('p'):
+        while True:
+            k = cv2.waitKey(1)
+            if k == ord('c'):
+                return
 
-cap.release()
-cv2.destroyAllWindows()
+def predict(frame):
+    cannyImage = getCannyImage(frame[ytop:ybottom, xleft:xright],lowIntensity,highIntensity)
+    predImage = cv2.resize(cannyImage, (imageSize, imageSize))
+    pred = model.predict(predImage.reshape(1, predImage.shape[0], predImage.shape[1], 1))
+    pred_cls = np.argmax(pred)
+    value = getCorrectClass(pred_cls,classMap)
+    return cannyImage,value
+
+model = load_model("FinalModel.h5")
+
+classMap = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, 'EQUALS':6, 'NONE':7, 'SUM':8}
+coord = [[50,300],[100,400]]
+white = (255, 255, 255)
+rectangleThickness = 2
+fontThickness = 2
+lowIntensity = 50
+highIntensity = 150
+imageSize = 100
+waitTime = 3
+displayResultTime = 2
+timeRemainingPosition = (10,80)
+outcomePosition = (30,80)
+font=cv2.FONT_HERSHEY_SIMPLEX
+value = 'NONE'
+
+cap = cv2.VideoCapture(0)
+while True:
+    xleft = coord[0][0]
+    xright = coord[0][1]
+    ytop = coord[1][0]
+    ybottom = coord[1][1]
+    ret, frame = cap.read()
+    if ret:
+        ptime = getTime()
+        while(getTime()-ptime<waitTime):
+            ret, frame = cap.read()
+            if ret:
+                timeRemaining = str(round(waitTime - getTime() + ptime,1))
+                
+                cv2.rectangle(frame, (xleft, ytop), (xright, ybottom), white, rectangleThickness)
+                cv2.putText(frame, " Capturing in: " + timeRemaining ,timeRemainingPosition, font, 1.2, white, fontThickness, cv2.LINE_AA)
+                cv2.imshow('frame', frame)
+                keyCheck()
+        ptime = getTime()
+        while(getTime()-ptime<displayResultTime):
+            ret, frame = cap.read()
+            if ret:
+                # predict the move made
+                cannyImage , value = predict(frame)
+                cv2.rectangle(frame, (xleft, ytop), (xright, ybottom), white, rectangleThickness)
+                cv2.putText(frame, " Outcome: " + str(value),outcomePosition, font, 1.2, white, fontThickness, cv2.LINE_AA)
+                cv2.imshow('frame', frame)
+                keyCheck()
+    keyCheck()
