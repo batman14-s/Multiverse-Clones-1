9bd80f40a8c60ed76e18039775ca1a9b6e617c2a
LazyXghost <B20123@students.iitmandi.ac.in>
2021-09-29 11:30:41 +0530

    Added ultimate model, working fabulously

--- live_gesturerecon.py
+++ live_gesturerecon.py
@@ -34,6 +34,13 @@ def keyCheck():
                 return
 
 
+def speak(a):
+    textspeech = pyttsx3.init()
+    speak = a
+    textspeech.say(speak)
+    textspeech.runAndWait()
+
+
 def predict(frame):
     cannyImage = getCannyImage(frame[ytop:ybottom, xleft:xright], lowIntensity, highIntensity)
     predImage = cv2.resize(cannyImage, (imageSize, imageSize))
@@ -43,12 +50,13 @@ def predict(frame):
     return cannyImage, value
 
 
-model = load_model("FinalModel.h5")
+model = load_model("UltimateUltimateHeaven.h5")
 
 classMap = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, 'EQUALS': 6, 'NONE': 7, 'SUM': 8}
 imageSize = 100
 coord = [[50, 300], [100, 400]]
 white = (255, 255, 255)
+red = (0,0,255)
 rectangleThickness = fontThickness = 2
 timeRemainingPosition = (10, 80)
 outcomePosition = (30, 80)
@@ -59,7 +67,7 @@ orders = []
 orderMenu = {'1': 'karela', '2': 'khichdi', '3': 'tinde', '4': 'tori', '5': 'ghiya'}
 
 lowIntensity = 50
-highIntensity = 150
+highIntensity = 100
 
 waitTime = 2
 resultCalculationTime = 2
@@ -82,6 +90,7 @@ while True:
                 if (timeRemaining <= 0):
                     timeRemaining = 0
                 cv2.rectangle(frame, (xleft, ytop), (xright, ybottom), white, rectangleThickness)
+                cv2.rectangle(frame, (xleft+40, ytop+30), (xright-40, ybottom-30), red, rectangleThickness)
                 cv2.putText(frame, " Capturing in: " + str(timeRemaining), timeRemainingPosition, font, 1.2, white,
                             fontThickness, cv2.LINE_AA)
                 cv2.imshow('frame', frame)
@@ -97,14 +106,12 @@ while True:
                 cannyImage, value = predict(frame)
                 order.append(value)
                 cv2.rectangle(frame, (xleft, ytop), (xright, ybottom), white, rectangleThickness)
+                cv2.rectangle(frame, (xleft+40, ytop+30), (xright-40, ybottom-30), red, rectangleThickness)
                 cv2.putText(frame, "Finding Outcome", outcomePosition, font, 1.2, white, fontThickness, cv2.LINE_AA)
                 cv2.imshow('frame', frame)
                 keyCheck()
         order = stats.mode(np.array(order))[0][0]
-        textspeech = pyttsx3.init()
-        speak = order
-        textspeech.say(speak)
-        textspeech.runAndWait()
+        speak(order)
 
         # displaying selectiong
         ptime = getTime()
@@ -119,7 +126,11 @@ while True:
                 keyCheck()
 
     if (order == 'SUM'):
+        print('Total no of orders are:', len(orders))
         print("Orders are:", orders)
+    elif(order == 'EQUALS'):
+        print("Orders are:", orders)
+        print('Order placed')
         cap.release()
         cv2.destroyAllWindows()
         exit()
@@ -128,9 +139,5 @@ while True:
     elif order != 'NONE':
         orders.append(orderMenu[order])
 
-    print(orders)
     keyCheck()
-    textspeech = pyttsx3.init()
-    speak = orders
-    textspeech.say(speak)
-    textspeech.runAndWait()
+    speak(orders)
